5cab5be5c4abd708eab93e8c2ae5624673e1be48
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-06 21:04:15 +0300

    added table describing education

--- app/about.tsx
+++ app/about.tsx
@@ -1,31 +1,48 @@
 import React from "react";
-import Link from "next/link";
+import Image from "next/image";
 
 function About() {
   return (
     <section id="about" className="mt-0 font-semibold md:mt-6">
       <h1 className="py-2 font-extrabold">About.</h1>
-      <div className="flex basis-full flex-col justify-center space-y-6">
-        <p>
-          Hailing from the coastal city of Mombasa, I am currently a thriving
-          freelancer. My exemplary service both as developer and freelance
-          technician provides a unique match to the needs of my clients. I excel
-          in troubleshooting and maintaining computer systems, security cameras
-          and networking.
-        </p>
-        <p>
-          My goal is not just to meet your expectation, but to exceed while
-          driving through with unparalleled success. Whether it is a website for
-          your business, or installation, you can count on me to deliver the
-          result to you.
-        </p>
-        <p>
-          Would you like to learn more about my experience? You can download my
-          resume{" "}
-          <Link href="#" className="text-indigo-400">
-            here!
-          </Link>
-        </p>
+
+      <div className="flex basis-full flex-col justify-center space-y-6 pt-4">
+        <div>
+          <h3 className="pb-1 text-xl font-semibold">Background</h3>
+          <p>
+            I&apos;m a passionate software engineer with a strong background in
+            developing robust and scalable web applications. Born and raised in
+            a small town, I developed an early interest in technology and
+            problem-solving. My curiosity and determination led me to pursue a
+            career in this field.
+          </p>
+        </div>
+        <div>
+          <h3 className="pb-1 text-xl font-semibold text-secondary-clr">
+            Education
+          </h3>
+          <table className="mb-4 w-full border-collapse rounded-md ring-2 ring-800">
+            <thead>
+              <tr className="bg-800">
+                <th className="px-4 py-2 text-left">Degree</th>
+                <th className="px-4 py-2 text-left">Institution</th>
+                <th className="px-4 py-2 text-left">Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className=" mx-auto divide-x-2 divide-800">
+                <td className="px-4 py-2">Bachelor of BIT</td>
+                <td className="px-4 py-2">JKUAT</td>
+                <td className="px-4 py-2">2016</td>
+              </tr>
+              <tr className="divide-x-2 divide-800 bg-gray-800/50">
+                <td className="px-4 py-2">High School Certificate</td>
+                <td className="px-4 py-2">Nyamagwa Boys&apos; School</td>
+                <td className="px-4 py-2">2009</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
