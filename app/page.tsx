a657b9a482eb9213b381c4dbb5376240297847ee
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-08 20:28:30 +0300

    added when network connection is off it provides a alert message

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,19 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isContentCached, setIsContentCached] = useState(false);
   const [cachedContent, setCachedContent] = useState(null);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
   useEffect(() => {
     // disable scrolling
     document.body.style.overflow = "hidden";
+
+    setIsOnline(navigator.onLine);
+
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
 
     const cachedContent = localStorage.getItem("motanya-content");
 
@@ -27,23 +36,35 @@ export default function App() {
 
     //  enable scrolling after data is fetched
     document.body.style.overflow = "auto";
+
+    // remove event-listeners if component mounts
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
   }, []);
   return isLoading ? (
     <SkeletonUI />
-  ) : isContentCached ? (
-    <CachedContent
-      cachedContent={
-        {
-          html: "This is the content to cache",
-          meta: {
-            title: "Cornelius | Portfolio",
-            description:
-              "Welcome to my portfolio website! I'm a passionate web developer with expertise in React, Next.js, and other modern web technologies. Browse my projects and learn more about my skills and experience.",
-          },
-        } || ""
-      }
-    />
+  ) : isOnline ? (
+    isContentCached ? (
+      <CachedContent
+        cachedContent={
+          {
+            html: "This is the content to cache",
+            meta: {
+              title: "Cornelius | Portfolio",
+              description:
+                "Welcome to my portfolio website! I'm a passionate web developer with expertise in React, Next.js, and other modern web technologies. Browse my projects and learn more about my skills and experience.",
+            },
+          } || ""
+        }
+      />
+    ) : (
+      <Home />
+    )
   ) : (
-    <Home />
+    <div>
+      <p>You are currently offline. Please check your internet connection.</p>
+    </div>
   );
 }
