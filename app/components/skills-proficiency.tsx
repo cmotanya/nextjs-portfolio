89966eb7b7e84bd876b00a8d1bd7e8f484bba78a
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-26 13:46:32 +0300

    removed IconStar and added rounded borders

--- app/components/skills-proficiency.tsx
+++ app/components/skills-proficiency.tsx
@@ -23,54 +23,12 @@ function ProfessionalSkills() {
         toggleDrawer={toggleDrawer}
         skillName="Programming:"
       >
-        <SkillItem
-          text="HTML"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarHalfFilled />}
-        />
-        <SkillItem
-          text="CSS"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarHalfFilled />}
-        />
-        <SkillItem
-          text="JavaScript"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarHalfFilled />}
-          icon5={<IconStar />}
-        />
-        <SkillItem
-          text="AstroJS"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStar />}
-          icon5={<IconStar />}
-        />
-        <SkillItem
-          text="ReactJS"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStar />}
-          icon5={<IconStar />}
-        />
-        <SkillItem
-          text="NextJS"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStar />}
-          icon5={<IconStar />}
-        />
+        <SkillItem text="HTML" />
+        <SkillItem text="CSS" />
+        <SkillItem text="JavaScript" />
+        <SkillItem text="AstroJS" />
+        <SkillItem text="ReactJS" />
+        <SkillItem text="NextJS" />
       </SkillDrawer>
 
       <SkillDrawer
@@ -78,38 +36,10 @@ function ProfessionalSkills() {
         isOpen={isDrawerOpen === "CCTV, Biometric and Access Controls:"}
         toggleDrawer={toggleDrawer}
       >
-        <SkillItem
-          text="Site Survey"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="Installation"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="Maintenance"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="Remote View"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
+        <SkillItem text="Site Survey" />
+        <SkillItem text="Installation" />
+        <SkillItem text="Maintenance" />
+        <SkillItem text="Remote View" />
       </SkillDrawer>
 
       <SkillDrawer
@@ -117,46 +47,11 @@ function ProfessionalSkills() {
         isOpen={isDrawerOpen === "Networking:"}
         toggleDrawer={toggleDrawer}
       >
-        <SkillItem
-          text="Basic Set-up"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="Troubleshooting"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="IP Addressing"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="Wireless Config"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
-        <SkillItem
-          text="Cabling"
-          icon1={<IconStarFilled />}
-          icon2={<IconStarFilled />}
-          icon3={<IconStarFilled />}
-          icon4={<IconStarFilled />}
-          icon5={<IconStarFilled />}
-        />
+        <SkillItem text="Basic Set-up" />
+        <SkillItem text="Troubleshooting" />
+        <SkillItem text="IP Addressing" />
+        <SkillItem text="Wireless Config" />
+        <SkillItem text="Cabling" />
       </SkillDrawer>
     </div>
   );
@@ -195,7 +90,7 @@ function SkillDrawer({
             animate={{ opacity: isOpen ? 1 : 0, height: isOpen ? "auto" : 0 }}
             exit={{ opacity: 0, height: 0 }}
             transition={{ duration: 0.3, delay: 0.2 }}
-            className="grid grid-flow-row gap-4 pt-2 text-400 group-hover:text-neutral-600 md:grid-cols-2"
+            className="flex flex-wrap gap-3 pt-2 text-400 group-hover:text-neutral-600"
           >
             {children}
           </motion.div>
@@ -205,29 +100,10 @@ function SkillDrawer({
   );
 }
 
-function SkillItem({
-  text,
-  icon1,
-  icon2,
-  icon3,
-  icon4,
-  icon5,
-}: {
-  text: string;
-  icon1: React.ReactNode;
-  icon2: React.ReactNode;
-  icon3: React.ReactNode;
-  icon4: React.ReactNode;
-  icon5: React.ReactNode;
-}) {
+function SkillItem({ text }: { text: string }) {
   return (
-    <div className="mx-auto flex gap-2 hover:text-neutral-400">
-      <span className="inline-block min-w-28">{text}</span>
-      <span className="hover:text-neutral-300">{icon1}</span>
-      <span>{icon2}</span>
-      <span>{icon3}</span>
-      <span>{icon4}</span>
-      <span>{icon5}</span>
+    <div className="flex hover:text-neutral-400">
+      <span className="rounded-full px-2 py-1 ring-2 ring-700">{text}</span>
     </div>
   );
 }
