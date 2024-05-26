3bb87ff6e734c14169231d7ac8171e5585a3c41f
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-26 20:32:12 +0300

    changed confetti

--- app/components/welcomePopUp.tsx
+++ app/components/welcomePopUp.tsx
@@ -13,49 +13,49 @@ const WelcomePopup = () => {
 
   useEffect(() => {
     const hasVisited = localStorage.getItem("visited");
+    const now = Date.now();
+    const lastVisitTime = localStorage.getItem("lastVisitedTime");
 
     const duration = 30 * 100;
     const end = Date.now() + duration;
 
-    if (!hasVisited) {
-      const canvas = canvasRef.current;
-      if (canvas) {
-        const myConfetti = confetti.create(canvas, {
-          resize: true,
-          useWorker: true,
-        });
-
-        (function timestamp() {
-          myConfetti({
-            particleCount: 7,
-            spread: 100,
-            angle: 60,
-            startVelocity: 30,
-            origin: { x: 0 },
-          });
-          myConfetti({
-            particleCount: 7,
-            spread: 100,
-            angle: 120,
-            startVelocity: 30,
-            origin: { x: 1 },
+    if (!lastVisitTime || now - parseInt(lastVisitTime) < 24 * 60 * 60 * 1000) {
+      if (!hasVisited) {
+        const canvas = canvasRef.current;
+        if (canvas) {
+          const myConfetti = confetti.create(canvas, {
+            resize: true,
+            useWorker: true,
           });
 
-          if (Date.now() < end) {
-            requestAnimationFrame(timestamp);
-          }
-        })();
+          (function timestamp() {
+            myConfetti({
+              particleCount: 5,
+              spread: 360,
+              angle: 60,
+              startVelocity: 30,
+              origin: { x: Math.random(), y: Math.random() - 0.2 },
+            });
 
-        localStorage.setItem("visited", "true");
+            if (Date.now() < end) {
+              requestAnimationFrame(timestamp);
+            }
+          })();
+
+          localStorage.setItem("visited", "true");
+        }
       }
     }
-  });
+  }, []);
   return (
     <div
       id="welcome-popup"
       className="fixed inset-0 z-50 m-auto max-h-fit max-w-[90%] rounded-md bg-800 p-4 md:max-w-md"
     >
-      <canvas ref={canvasRef} className="absolute inset-0 z-0"></canvas>
+      <canvas
+        ref={canvasRef}
+        className="absolute left-1/2 top-1/2 -z-40 h-full w-full -translate-x-1/2 -translate-y-1/2"
+      ></canvas>
       <div className="space-y-5">
         <h2 className="text-2xl font-semibold uppercase">Welcome</h2>
         <p>
