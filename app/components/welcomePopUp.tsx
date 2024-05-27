5965c9f0835b0532776ad04326e17f7c2f185265
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-27 19:23:25 +0300

    changed paragraph in welcome popup

--- app/components/welcomePopUp.tsx
+++ app/components/welcomePopUp.tsx
@@ -62,18 +62,15 @@ const WelcomePopup = () => {
         className="absolute left-1/2 top-1/2 z-[100] h-full w-full -translate-x-1/2 -translate-y-1/2"
       ></canvas>
       <div className="relative z-[999999] max-h-fit max-w-[90%] rounded-md bg-800 p-4 md:max-w-md">
-        <div className="space-y-3">
-          <h2 className="text-center text-2xl font-semibold uppercase">
+        <div className="">
+          <h2 className=" mb-4 text-center text-2xl font-semibold uppercase">
             Welcome
           </h2>
-          <p className="text-balance">
-            Thank you for visiting my portfolio. I am delighted to have you
-            here!
-          </p>
+          <p className="text-balance">Thank you for visiting my portfolio.</p>
           <p>Please feel free to reach out for comments and questions.</p>
           <button
             onClick={handleClose}
-            className="mx-auto flex w-full justify-center rounded-full bg-red-600 px-5 py-2 text-200 transition active:-translate-y-2 md:w-fit md:py-1"
+            className="mx-auto mt-6 flex w-full justify-center rounded-full bg-red-600 px-5 py-2 text-200 transition active:-translate-y-2 md:max-w-fit md:py-1"
           >
             Close
           </button>
