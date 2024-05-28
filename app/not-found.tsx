d78330431cb1a980f2856a57d15bdf4ce8a9e3cc
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-28 20:37:42 +0300

    added padding not-found-icon

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,12 +3,12 @@ import NotFoundIcon from "../public/svg/not-found-icon.svg";
 
 function NotFound() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-center text-gray-300">
-      <div className="mb-8">
+    <div className="flex min-h-dvh flex-col items-center justify-center overflow-hidden bg-gray-900 text-center text-gray-300">
+      <div className="pb-10">
         {/* <h1 className="text-9xl font-bold">404</h1> */}
         <NotFoundIcon />
       </div>
-      <div className="mb-12">
+      <div className="pb-10">
         <p className="text-xl">
           The page you are looking for could not be found.
         </p>
