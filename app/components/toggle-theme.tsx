cabaf2b19609713e4ef20dc498e631c4e208ff90
Cornelius Motanya <corneliusmot@yahoo.com>
2024-05-29 07:20:58 +0300

    changebg-color for light theme

--- app/components/toggle-theme.tsx
+++ app/components/toggle-theme.tsx
@@ -7,12 +7,12 @@ export const ThemeToggle = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="absolute  left-4 top-1/2 -translate-y-1/2 z-[500] text-6xl"
+      className="absolute  left-4 top-1/2 z-[500] -translate-y-1/2 text-6xl"
     >
       {theme == "dark" ? (
         <IconSun className="xs:size-8 md:size-6" />
       ) : (
-        <IconMoonFilled className="xs:size-8 md:size-6" />
+        <IconMoonFilled className="xs:size-8 text-900 md:size-6" />
       )}
     </button>
   );
