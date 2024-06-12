266c784a1b58ff76d0e8d37c09a872f184007e5a
Cornelius Motanya <corneliusmot@yahoo.com>
2024-06-12 19:06:02 +0300

    removed errors in toaster

--- app/components/aboutButtons.tsx
+++ app/components/aboutButtons.tsx
@@ -21,14 +21,14 @@ const AboutButtons = () => {
       link.click();
       document.body.removeChild(link);
 
-      toast.promise(promise, {
-        loading: "Downloading",
-        success: (data: { name: string }) => {
-          return data.name + " downloaded!";
-        },
-        error: () => "Something went wrong!",
-      });
-      // toast.success("Downloaded!");
+      // toast.promise(promise, {
+      //   loading: "Downloading",
+      //   success: (data: { name: string }) => {
+      //     return data.name + " downloaded!";
+      //   },
+      //   error: () => "Something went wrong!",
+      // });
+      toast.success("Downloaded!");
     }, 3000);
   }, []);
 
