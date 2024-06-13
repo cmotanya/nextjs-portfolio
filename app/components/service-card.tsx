2a172b0296b9c806d967ddb74dfb6ea5512c9d98
Cornelius Motanya <corneliusmot@yahoo.com>
2024-06-13 21:15:08 +0300

    changed bg-color

--- app/components/service-card.tsx
+++ app/components/service-card.tsx
@@ -78,27 +78,30 @@ const ServiceCard = () => {
       <div className="mt-2 flex flex-col gap-4 md:flex-row">
         <div
           ref={webCardRef}
-          className="invisible flex rotate-6 scale-75 transform flex-col justify-between rounded-sm bg-secondary-clr p-3 opacity-0 shadow-xl transition duration-700 ease-in-out dark:bg-800"
+          className="invisible flex rotate-6 scale-75 transform flex-col justify-between rounded-md border-2 border-400 bg-transparent p-3 opacity-0 shadow-xl transition duration-700 ease-in-out dark:border-800 dark:bg-slate-800/40"
         >
-          <p className="flex items-center justify-center gap-3 pb-3 text-xl uppercase text-bright-clr dark:text-600">
+          <p className="flex items-center justify-center gap-3 bg-transparent pb-3 text-xl uppercase text-bright-clr backdrop-blur-lg dark:text-400">
             <IconDeviceLaptop /> web
           </p>
           <div className="space-y-2 text-800 *:leading-tight dark:text-400">
-            <p>
-              <strong>Sing-page website:</strong> Perfect for small businesses
-              or personal projects.
+            <p className="flex">
+              <IconCheckbox size={53} className="-mt-[0.8rem] mr-2" />
+              <span className="text-700 dark:text-500">
+                Sing-page website: Perfect for small businesses or personal
+                projects.
+              </span>
             </p>
-            <p>
-              <strong>Portfolio website:</strong> Showcase your work in a
-              stunning online portfolio.
-            </p>
-            <p>
-              <strong>Business website:</strong> Comprehensive sites with
-              multiple pages to represent your business.
+
+            <p className="flex">
+              <IconCheckbox size={53} className="-mt-[0.8rem] mr-2" />
+              <span className="text-700 dark:text-500">
+                Portfolio website:Showcase your work in a stunning online
+                portfolio.
+              </span>
             </p>
           </div>
           <div className="mt-5 flex justify-center">
-            <button className="w-full transform rounded-full bg-primary-clr px-4 py-2 uppercase text-200 transition hover:text-300 active:-translate-y-2 dark:bg-900 dark:text-400 dark:hover:text-600 md:w-fit">
+            <button className="w-full transform rounded-full bg-primary-clr px-4 py-2 uppercase text-300 transition active:-translate-y-2 dark:bg-900 dark:text-400 dark:hover:text-600 md:w-fit">
               Get Started Today
             </button>
           </div>
@@ -106,9 +109,9 @@ const ServiceCard = () => {
 
         <div
           ref={netCardRef}
-          className="flex transform flex-col justify-between rounded-md bg-transparent p-3 shadow-xl transition duration-700 ease-in-out dark:bg-slate-800/40"
+          className="invisible flex rotate-6 scale-75 transform flex-col justify-between rounded-md border-2 border-400 bg-transparent p-3 opacity-0 shadow-xl transition duration-700 ease-in-out dark:border-800 dark:bg-slate-800/40"
         >
-          <p className="flex items-center justify-center gap-3 pb-3 bg-transparent backdrop-blur-lg text-xl uppercase text-bright-clr dark:text-400">
+          <p className="flex items-center justify-center gap-3 bg-transparent pb-3 text-xl uppercase text-bright-clr backdrop-blur-lg dark:text-400">
             <IconActivity /> cctv and networking
           </p>
 
@@ -130,7 +133,7 @@ const ServiceCard = () => {
             </p>
           </div>
           <div className="mt-5 flex justify-center">
-            <button className="w-full transform rounded-full bg-primary-clr px-4 py-2 uppercase text-300 transition hover:text-300 active:-translate-y-2 dark:bg-900 dark:text-400 dark:hover:text-600 md:w-fit">
+            <button className="w-full transform rounded-full bg-secondary-clr px-4 py-2 font-semibold uppercase transition active:-translate-y-2 dark:bg-900 dark:text-400 dark:hover:text-600 md:w-fit">
               connect now
             </button>
           </div>
